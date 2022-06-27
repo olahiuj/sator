1846890fade35aa54bba2ba5d6866b866aa85810
jpwang <jpwang@smail.nju.edu.cn>
2022-06-27 16:52:28 +0800

    implement translation to zh

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ INCS := $(shell find include -maxdepth 1 -name "*.h")
 DEPS := $(INCS) $(SRCS)
 CFLAGS := -O1 -std=gnu11 -Wall -Werror -Wno-unused-result -Wno-unused-value -Wno-unused-variable
 
-.PHONY clean
+.PHONY: clean
 
 run:
 	gcc $(CFLAGS) $(SRCS) -o build/sator
--- src/sator.c
+++ src/sator.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static const char fmt[] = "notify-send \"%s\"";
+#define NR_BUF 4096
 
-int main(int argc, char *argv[]) {
-  FILE *pf = popen("xclip -o", "r");
+static const char notify_fmt[] = "notify-send \"%s\"";
+static const char trans_fmt[] = "trans --brief :zh \"%s\"";
+
+FILE *Popen(const char *cmd, const char *type) {
+  FILE *pf = popen(cmd, type);
   if (pf == NULL) {
     perror("popen");
     exit(1);
   }
+  return pf;
+}
 
-  static char word[NR_BUF];
-  fgets(word, sizeof(word) - 1, pf);
+int main(int argc, char *argv[]) {
+  FILE *pf = Popen("xclip -o", "r");
+  static char word_en[NR_BUF];
+  fgets(word_en, sizeof(word_en) - 1, pf);
   pclose(pf);
 
-  static char notify[NR_BUF];
-  snprintf(notify, sizeof(fmt) - 1, fmt, word);
+  static char trans[NR_BUF * 2];
+  snprintf(trans, sizeof(trans) - 1, trans_fmt, word_en);
+  pf = Popen(trans, "r"); 
+  static char word_zh[NR_BUF];
+  fgets(word_zh, sizeof(word_zh) - 1, pf);
+  pclose(pf);
+
+  static char notify[NR_BUF * 2];
+  snprintf(notify, sizeof(notify) - 1, notify_fmt, word_zh);
   system(notify);
 
   return 0;
